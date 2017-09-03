91b2c90d3ae61c9806762df78409f8e21f37e3a4
John <CrystallineEntity@users.noreply.github.com>
2017-09-02 21:20:59 -0400

    harder better faster stronger

--- problem10.go
+++ problem10.go
@@ -8,6 +8,8 @@ import (
 
 func sieve(limit int) []bool {
     primes := make([]bool, limit+1)    
+    limitsqrt := int(math.Sqrt(float64(limit)))
+    
     
     for x:=0; x<=limit; x++ {
         if x < 2 {
@@ -17,7 +19,7 @@ func sieve(limit int) []bool {
         }
     }
     
-    for i := 0; i <= int(math.Sqrt(float64(limit))); i++ {
+    for i := 0; i <= limitsqrt; i++ {
         if primes[i] {
             for j:=i*i; j<limit; j+=i {
                 primes[j] = false;
