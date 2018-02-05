9fe1db4d37603d6c79454221d04dbe0468371381
ComputerMachine <7267435+ComputerMachine@users.noreply.github.com>
2018-02-04 22:36:36 -0800

    Update problem21.py

--- problem21.py
+++ problem21.py
@@ -1,32 +1,21 @@
 from itertools import count
 
 
-def d(n):
-	return sum(x for x in range(1, n) if n % x == 0)
+def propdiv(n):
+    return sum(x for x in range(1, n//2+1) if n % x == 0)
 
 amicables = []
-addpair = lambda a, b: amicables.extend([a, b])
 
-for n in count(1):
-    a = d(n)
-    b = d(a)
-    if b == n:
-        if a in amicables or b in amicables:
+for k in count(1):
+    a = propdiv(k)
+    b = propdiv(a)
+    if b == k:
+        if b in amicables or a in amicables:
             continue
         if a != b:
-            addpair(a, b)
-    try:
-        if max(amicables) >= 10000:
-            break
-    except ValueError:
-        # amicables is empty
-        pass
-
-while 1:
-    m = max(amicables)
-    if m >= 10000:
-        amicables.remove(m)
-    else:
-        break
-
+            if a > 10000 or b > 10000:
+                break
+            print (amicables)
+            amicables.extend([a, b])
+            
 print (sum(amicables))
